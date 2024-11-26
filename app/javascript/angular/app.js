bc0c3b914b7274a70a36e8c4ead4e9c28415b539
minthakarnn <minthakarn.w@gmail.com>
2024-11-26 14:07:59 +0700

    angular update

--- app/javascript/angular/app.js
+++ app/javascript/angular/app.js
@@ -1,12 +1,12 @@
-angular.module('myApp', [])
-  .config(['$httpProvider', function($httpProvider) {
-    // ตั้งค่า CSRF Token สำหรับ HTTP requests ทั้งหมด
-    $httpProvider.defaults.headers.common['X-CSRF-Token'] =
-      document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-  }]);
+// angular.module('myApp', [])
+//   .config(['$httpProvider', function($httpProvider) {
+//     // ตั้งค่า CSRF Token สำหรับ HTTP requests ทั้งหมด
+//     $httpProvider.defaults.headers.common['X-CSRF-Token'] =
+//       document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+//   }]);
 
-// โหลด Controllers
-require('./controllers/employee_controller');
-require('./controllers/employee_form_controller');
-require('./controllers/employee_show_controller');
-require('./controllers/employee_edit_form_controller');
+// // โหลด Controllers
+// require('./controllers/employee_controller');
+// require('./controllers/employee_form_controller');
+// require('./controllers/employee_show_controller');
+// require('./controllers/employee_edit_form_controller');
