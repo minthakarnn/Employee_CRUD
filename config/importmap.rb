e27dbf216dce52e93586f5044d2a6b6e045ae6d4
minthakarnn <minthakarn.w@gmail.com>
2024-11-26 14:41:28 +0700

    angular update

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,6 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sweetalert2" # @11.14.4
+
+pin_all_from 'app/javascript/controllers', under: 'controllers'
